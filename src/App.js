a26526abf43d2eb6b3f462c336201f7085b0cdea
Nicholas <nicklzw97@hotmail.com>
2021-06-25 13:24:53 +0800

    add Modal and Backdrop to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // seperate the components into different js file
 import Todo from "./components/Todo";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Todo text="hello" />
       <Todo text="cool" />
       <Todo text="naise" />
+      <Modal />
+      <Backdrop />
     </div>
   ); //returns jsx that can be run by browser
 }
