69615e790d21e5712f3a19cb298ea356b40fad6b
Nicholas <nicklzw97@hotmail.com>
2021-06-25 16:56:41 +0800

    addd css classes to Navigation

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import classes from "./Navigation.module.css";
 
 function Navigation() {
   return (
-    <header>
-      <header>
+    <header className={classes.header}>
+      <header className={classes.logo}>
+        Hello
         <nav>
           <ul>
             <li>
