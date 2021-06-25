7a4a7fba7ff48ee2536240d0a830b6fbce49ad27
Nicholas <nicklzw97@hotmail.com>
2021-06-25 13:24:34 +0800

    update Backdrop and Modal

---
+++ src/components/Backdrop.js
@@ -0,0 +1,5 @@
+function Backdrop(props) {
+  return <div className="backdrop"></div>;
+}
+
+export default Backdrop;
---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal(props) {
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <button classname="btn btn--alt">Cancel</button>
+      <button classname="btn">Confirm</button>
+    </div>
+  );
+}
+
+export default Modal;
