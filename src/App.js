8e9a7695f84feec028e4a8e15754dbffee803e50
Nicholas <nicklzw97@hotmail.com>
2021-06-25 15:11:06 +0800

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import AllMeetups from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favourites from "./pages/Favourities";
+import { Route, Switch } from "react-router-dom";
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      {/* switch ensures that only 1 page is shown at any point in time */}
+      <Switch>
+        {/* route helps to display the appropriate js file when the url is pointing at it */}
+        <Route path="/" exact>
+          <AllMeetups />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+        <Route path="/favourites">
+          <Favourites />
+        </Route>
+      </Switch>
     </div>
   );
 }
