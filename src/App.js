f20b5ebbd4ed9cb28aba4fb11ffe39ccbe07bf88
Nicholas <nicklzw97@hotmail.com>
2021-06-25 13:18:15 +0800

    update src files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+// seperate the components into different js file
+import Todo from "./components/Todo";
+
 function App() {
-  return console.log("Hello");
+  return (
+    <div>
+      <h1>To-do List!</h1>
+      <Todo text="hello" />
+      <Todo text="cool" />
+      <Todo text="naise" />
+    </div>
+  ); //returns jsx that can be run by browser
 }
 
 export default App;
